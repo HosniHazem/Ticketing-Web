e86c4076b42ef9a388dd99b6baf844abe0673aa2
HosniHazem <hazem.hosni@aiesec.net>
2022-03-11 14:57:29 +0100

    Migrations Creation

---
+++ database/migrations/2022_03_11_135113_create__role_permission.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolePermission extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_role_permission', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_role_permission');
+    }
+}
